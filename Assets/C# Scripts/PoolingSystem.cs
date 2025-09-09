08e681244f542b6b698182b63f5bb812b4e2387a
FirePixel8422 <bmtminecraft@hotmail.com>
2025-09-09 10:46:07 +0200

    Pooling System V2
    Updated Pooling System to support any type and is now extremely efficient.

--- Assets/C# Scripts/PoolingSystem.cs
+++ Assets/C# Scripts/PoolingSystem.cs
@@ -3,55 +3,135 @@ using System.Runtime.CompilerServices;
 using UnityEngine;
 
 
-public class PoolingSystem
+public class PoolingSystem<T> where T : Component
 {
-    [SerializeField] private GameObject pooledPrefab;
+    [SerializeField] private T pooledPrefab;
 
     [SerializeField] private int startCapacity;
     [SerializeField] private bool dynamicRefill;
 
-    private List<GameObject> pooledList = new List<GameObject>();
+    private List<T> allObjects;
+    private Queue<T> availableObjects;
+
+#if UNITY_EDITOR
+    private Transform objHolderParent;
+#endif
 
 
-    public PoolingSystem(int startCapacity, bool dynamicRefill)
+    public PoolingSystem(T pooledPrefab, int startCapacity, bool dynamicRefill)
     {
+        this.pooledPrefab = pooledPrefab;
         this.startCapacity = startCapacity;
         this.dynamicRefill = dynamicRefill;
+
+        if (pooledPrefab == null)
+        {
+            DebugLogger.LogError("Pooling System of object: '" + pooledPrefab.name + "' pooledPrefab is null!!");
+            return;
+        }
+
+        Initialize();
     }
-
-
-    public void Initialize()
+    private void Initialize()
     {
-        pooledList = new List<GameObject>();
+        allObjects = new List<T>(startCapacity);
+        availableObjects = new Queue<T>(startCapacity);
+
+#if UNITY_EDITOR
+        objHolderParent = new GameObject("DEBUG_PoolingSystemHolder: " + pooledPrefab.name).transform;
+#endif
+
         for (int i = 0; i < startCapacity; i++)
         {
-            GameObject pooledObject = GameObject.Instantiate(pooledPrefab, Vector3.zero, Quaternion.identity);
-            pooledObject.SetActive(false);
+            T obj = CreateNewObject();
 
-            pooledList.Add(pooledObject);
+            allObjects.Add(obj);
+            availableObjects.Enqueue(obj);
         }
     }
 
-    public GameObject GetPulledObj(int index, Vector3 pos, Quaternion rot)
+    public T GetPulledObject(Vector3 pos, Transform parent = null)
     {
-        foreach (GameObject obj in pooledList)
+        return GetPulledObject(pos, Quaternion.identity, Vector3.one, parent);
+    }
+
+    public T GetPulledObject(Vector3 pos, Quaternion rot, Transform parent = null)
+    {
+        return GetPulledObject(pos, rot, Vector3.one, parent);
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T GetPulledObject(Vector3 pos, Quaternion rot, Vector3 scale, Transform parent = null)
+    {
+        if (availableObjects.Count > 0)
         {
-            if (obj.activeInHierarchy == false)
-            {
-                obj.SetActive(true);
-                obj.transform.SetPositionAndRotation(pos, rot);
-                return obj;
-            }
+            T obj = availableObjects.Dequeue();
+
+            return SetupObject(obj, pos, rot, scale, parent);
         }
 
         if (dynamicRefill)
         {
-            GameObject newPooledObject = GameObject.Instantiate(pooledPrefab, Vector3.zero, Quaternion.identity);
-            pooledList.Add(newPooledObject);
+            T obj = CreateNewObject();
 
-            return newPooledObject;
+            allObjects.Add(obj);
+
+            return SetupObject(obj, pos, rot, scale, parent);
+        }
+        else
+        {
+            DebugLogger.LogError("Pooling System of object: '" + pooledPrefab.name + "' exceeded limits, resizing is disabled, FIX THIS");
         }
 
         return null;
+    }
+
+    public void ReleasePooledObject(T obj, bool clearParent = false)
+    {
+        obj.gameObject.SetActive(false);
+        if (clearParent)
+        {
+#if UNITY_EDITOR
+            obj.transform.SetParent(objHolderParent);
+#else
+            obj.transform.parent = null;
+#endif
+        }
+
+        availableObjects.Enqueue(obj);
+    }
+
+
+
+    /// <summary>
+    /// Create (Instantiate) a new Object <typeparamref name="T"/> and set it inactive
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private T CreateNewObject()
+    {
+        T obj = Object.Instantiate(pooledPrefab);
+        obj.gameObject.SetActive(false);
+
+#if UNITY_EDITOR
+        obj.transform.SetParent(objHolderParent);
+#endif
+
+        return obj;
+    }
+
+    /// <summary>
+    /// Set object <typeparamref name="T"/> active set position rotation and optional parent
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    private T SetupObject(T obj, Vector3 pos, Quaternion rot, Vector3 scale, Transform parent = null)
+    {
+        obj.gameObject.SetActive(true);
+        obj.transform.SetPositionAndRotation(pos, rot);
+        obj.transform.localScale = scale;
+        if (parent != null)
+        {
+            obj.transform.SetParent(parent, true);
+        }
+        return obj;
     }
 }
--- Assets/C# Scripts/Utility/Float3.cs
+++ Assets/C# Scripts/Utility/Float3.cs
@@ -1,17 +1,20 @@
-﻿using Unity.Mathematics;
+﻿
 
 
-/// <summary>
-/// Helper struct for float3 containing constants like Up Forward and Zero
-/// </summary>
-public static class Float3
+namespace Unity.Mathematics
 {
-    public static float3 Zero => new float3(0f, 0f, 0f);
-    public static float3 One => new float3(1f, 1f, 1f);
-    public static float3 Up => new float3(0f, 1f, 0f);
-    public static float3 Down => new float3(0f, -1f, 0f);
-    public static float3 Left => new float3(-1f, 0f, 0f);
-    public static float3 Right => new float3(1f, 0f, 0f);
-    public static float3 Forward => new float3(0f, 0f, 1f);
-    public static float3 Backward => new float3(0f, 0f, -1f);
+    /// <summary>
+    /// Helper struct for float3 containing constants like Up Forward and Zero
+    /// </summary>
+    public static class Float3
+    {
+        public static float3 Zero => new float3(0f, 0f, 0f);
+        public static float3 One => new float3(1f, 1f, 1f);
+        public static float3 Up => new float3(0f, 1f, 0f);
+        public static float3 Down => new float3(0f, -1f, 0f);
+        public static float3 Left => new float3(-1f, 0f, 0f);
+        public static float3 Right => new float3(1f, 0f, 0f);
+        public static float3 Forward => new float3(0f, 0f, 1f);
+        public static float3 Backward => new float3(0f, 0f, -1f);
+    }
 }
