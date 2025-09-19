6736caae8d8f8a54e3cb8eccb5b4ced5712318d7
R4HK00N <robert.eddy.beltman@gmail.com>
2025-09-19 11:41:00 +0200

    lobby ui

--- Assets/UI/NewUI/Scripts/ButtonFunctions.cs
+++ Assets/UI/NewUI/Scripts/ButtonFunctions.cs
@@ -1,6 +1,9 @@
 using FirePixel.Networking;
 using System.Collections;
 using UnityEngine;
+using static UnityEditor.Rendering.CameraUI;
+using UnityEngine.UI;
+using TMPro;
 
 public class ButtonFunctions : MonoBehaviour
 {
@@ -8,6 +11,17 @@ public class ButtonFunctions : MonoBehaviour
     public Animator animator;
 
     public float[] animationWaitTime;
+
+    [SerializeField] private string enteredRoomCode;
+    public TMP_InputField codeInputfield;
+    public TMP_Text codeDisplay;
+
+    [Header("Armory")]
+    public GameObject[] armoryScreens;
+
+    public GameObject[] previewGuns;
+    public GameObject[] previewGunsAttachments;
+    int selectedGun;
 
     #region MainScreen
     public void GoToLobbyButton()
@@ -37,14 +51,36 @@ public class ButtonFunctions : MonoBehaviour
     #region LobbyScreens
     public void CreateRoom()
     {
+        //LobbyMaker.Instance.CreateLobbyAsync();
+        //codeDisplay.text = LobbyManager.LobbyCode;
+
         mainScreens[1].SetActive(false);
         mainScreens[2].SetActive(true);
-
-        //LobbyMaker.Instance.CreateLobbyAsync();
     }
     public void GoJoinRoomScreen()
     {
+        mainScreens[1].SetActive(false);
+        mainScreens[4].SetActive(true);
+    }
+    public async void JoinRoomByCode()
+    {
+        bool succes = await LobbyMaker.Instance.JoinLobbyByIdAsync(codeInputfield.text);
 
+        if (succes)
+        {
+            mainScreens[4].SetActive(false);
+            mainScreens[2].SetActive(true);
+        }
+    }
+    public async void QuickJoinRoom()
+    {
+        bool succes = await LobbyMaker.Instance.AutoJoinLobbyAsync();
+
+        if (succes)
+        {
+            mainScreens[4].SetActive(false);
+            mainScreens[2].SetActive(true);
+        }
     }
     public void StartGame()
     {
@@ -63,18 +99,49 @@ public class ButtonFunctions : MonoBehaviour
     }
     public void BackToLobbySearchJoin()
     {
-
+        mainScreens[4].SetActive(false);
+        mainScreens[1].SetActive(true);
     }
     #endregion
     #region Armory
-    // als je van weapon select gaat naar attachment vergeet niet de offset van -0.2 naar 0.2 te zetten van de cinemachinecamera
+    public void PreviewGunButton(int GunID)
+    {
+        foreach(GameObject gun in previewGuns)
+        {
+            gun.SetActive(false);
+        }
+        previewGuns[GunID].SetActive(true);
+        //previewGuns[GunID]
 
+        selectedGun = GunID;
+    }
+    public void CustomizeButton()
+    {
+        previewGunsAttachments[selectedGun].SetActive(true);
+        armoryScreens[0].SetActive(false);
+        armoryScreens[1].SetActive(true);
+    }
+    public void AttachmentButton()
+    {
+        
+    }
     //ALLE BACKBUTTONS HIERONDER
     public void GoBackFromArmory()
     {
         animator.SetInteger("UI", 2);
 
         mainScreens[3].SetActive(false);
+
+        foreach (GameObject gun in previewGuns)
+        {
+            gun.SetActive(false);
+        }
+    }
+    public void BackToGuns()
+    {
+        previewGunsAttachments[selectedGun].SetActive(false);
+        armoryScreens[1].SetActive(false);
+        armoryScreens[0].SetActive(true);
     }
     #endregion
 
@@ -83,5 +150,8 @@ public class ButtonFunctions : MonoBehaviour
         yield return new WaitForSeconds(time);
 
         mainScreens[3].SetActive(true);
+
+        selectedGun = 0;
+        previewGuns[0].SetActive(true);
     }
 }
--- Assets/UI/NewUI/Scripts/ClickAndRotate.cs
+++ Assets/UI/NewUI/Scripts/ClickAndRotate.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class ClickAndRotate : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 300f;
 
-    [SerializeField] private float rotX, rotY;
-    [SerializeField] private MinMaxFloat clampX, clampY;
+    [SerializeField] private MinMaxFloat clampX;
 
+    public float rotationSpeed = 5f;
+    private float rotationX = 0f;
+    private float rotationY = 0f;
 
     private void Update()
     {
+
         if (Input.GetMouseButton(0))
         {
-            print('f'); ;
+            // Get mouse movement
+            float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+            float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
 
-            // Use raw mouse delta, not Input.GetAxis, for smoother consistent input
-            float mouseX = Input.GetAxisRaw("Mouse X");
-            float mouseY = Input.GetAxisRaw("Mouse Y");            
+            // Update rotation values
+            rotationX += mouseY;
+            rotationY -= mouseX;
+            rotationX = Mathf.Clamp(rotationX, clampX.min, clampX.max);
 
-            transform.Rotate(Vector3.up, mouseX * rotationSpeed * Time.deltaTime);
-            transform.Rotate(Vector3.right, mouseY * rotationSpeed * Time.deltaTime);
-
-            Vector3 euler = transform.localEulerAngles;
-            euler.x = Mathf.Clamp(euler.x, clampX.min, clampX.max);
-            euler.y = Mathf.Clamp(euler.y, clampY.min, clampY.max);
-            euler.z = 0;
-            transform.localEulerAngles = euler;
+            // Apply rotation
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0f);
         }
     }
+
+
 }
